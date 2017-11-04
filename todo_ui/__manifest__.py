0b8452f2cd2cd4d4ed02116037057707aa710a48
Abdul Halim <abdulhalim.cu10@gmail.com>
2017-11-04 10:52:41 +0600

    todo view

---
+++ todo_ui/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "User interface improvement to the To-Do app",
+
+    'summary': """Todo, User UI Todo""",
+
+    'description': """User friendly features """,
+
+    'author': "Abdul Halim",
+    'website': "http://aristobd.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['todo_user'],
+
+    'application': True,
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/todo_view.xml',
+        'views/todo_menu.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ todo_ui/models/todo_model.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+# from odoo.exceptions import ValidationError
+# Import Referenceable models configuration to improve flaxibility of Refers to field
+from odoo.addons.base.res.res_request import referenceable_models
+
+
+class Tag(models.Model):
+    _name = 'todo.task.tag'
+    _description = 'To-do Tag'
+    # Enable heirarchic relationship features 
+    _parent_store = True
+    name = fields.Char('Name')
+    # _parent_name = 'parent_id'
+    # Parent-child tree relationships using a Many2one relationships
+    parent_id = fields.Many2one('todo.task.tag', 'Parent Tag', ondelete='restrict')
+    parent_left = fields.Integer('Parent Left', index=True)
+    parent_right = fields.Integer('Parent Right', index=True)
+
+
+class Stage(models.Model):
+    _name = 'todo.task.stage'
+    _description = 'To-do Stage'
+    _order = 'sequence,name'
+    
+    # String fields:
+    name = fields.Char('Name')
+    desc = fields.Text('Description')
+    state = fields.Selection(
+                [('draft', 'New'), ('open', 'Started'),
+                 ('done', 'Closed')], 'State')
+    docs = fields.Html('Documentation')
+
+    # Numeric Fields:
+    sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3, 2))
+
+    # Date fields:
+    date_effective = fields.Date('Effective Date')
+    date_changed = fields.Datetime('Last Changed')
+
+    # other fields:
+    fold = fields.Boolean('Folded?')
+    image = fields.Binary('Image')
+
+
+class TodoTask(models.Model):
+    _inherit = 'todo.task'
+    
+    # Refers to field that can either refer to a User or a Partner
+    # refers_to = fields.Reference(
+    #             [('res.user', 'User'), ('res.partner', 'Partner')], 'Refers to')
+    # Modifying referenceable model configuration to extend flaxibility
+    refers_to = fields.Reference(referenceable_models, 'Refers to')
+
+    stage_id = fields.Many2one('todo.task.stage', 'Stage')
+    # Tasl <-> Tag relation (positional args)
+    tag_ids = fields.Many2many('todo.task.tag',     # related model
+                'todo_task_tags_rel',               # relation table name for avoiding limitations
+                'task_id',                          # field for 'this' record
+                'tag_id',                           # field for 'other' record
+                string="Tags")
+    # We can instead use keyword arguments, which some people prefer for readability
+    # Task <-> Tag relation (keyword args)
+    # tag_ids = fields.Many2many(
+    #           comodel_name='todo.task.tag',       # related model
+    #           relation='todo_task_tag_rel',       # relation table name for avoiding limitations
+    #           column1='task_id',                  # field for 'this' record
+    #           column2='tag_id',                   # field for 'other' record
+    #           string='Tags')
+    
+    
+    # Compute field declaration and set a search function for search logic
+    # and inverse function for implementing write logic
+    stage_fold = fields.Boolean(string='Stage Folded?', 
+            compute='_compute_stage_fold',
+            search = '_search_stage_fold',
+            inverse = '_write_stage_fold')
+
+    # Related fields for auto handle computed fields
+    stage_state = fields.Selection(
+                related='stage_id.state',
+                string='Stage State'
+            )
+
+    # SQL constraints
+    _sql_constraints = [('todo_task_name_uniq',
+                    'UNIQUE (name, active)',
+                    'Task title must be unique!')]
+    # python constraints
+    # @api.constrains('name')
+    #  def _check_name_size(self):
+    #      for todo in self:
+    #          if len(todo.name) < 5:
+    #              raise ValidationError('Must have 5 chars!')
+
+    @api.depends('stage_id.fold')
+    def _compute_stage_fold(self):
+        for task in self:
+            task.stage_fold = task.stage_id.fold
+
+    def _search_stage_fold(self, operator, value):
+        return [('stage_id.fold', operator, value)]
+
+    def _write_stage_fold(self):
+        self.stage_id.fold = self.stage_fold
+
