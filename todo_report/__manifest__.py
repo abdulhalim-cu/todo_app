3323637eaf6e852c8b2426eec5d553973a2d0cef
Abdul Halim <abdulhalim.cu10@gmail.com>
2017-11-14 16:48:32 +0600

    report todo enhanced

--- todo_report/__manifest__.py
+++ todo_report/__manifest__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "todo_report",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
+    'name': "Todo Report",
+    'summary': """Todo, Report, Task""",
+    'description': """Report for To-Do Tasks.""",
+    'author': "Abdul Halim",
+    'website': "https://abdulhalim.pythonanywhere.com",
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'application': True,
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'reports/todo_report.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
